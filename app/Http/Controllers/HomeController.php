c0557ca854d645d1ca9da25a83a0a9f50ec9d3d0
ainea <ainea@localhost.localdomain>
2023-03-04 11:05:37 +0300

    admin profile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Redirect,Response;
+Use DB;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -24,5 +28,40 @@ class HomeController extends Controller
     public function index()
     {
         return view('admin.index');
+    }
+    public function profile(){
+        $profile=Auth()->user();
+        // return $profile;
+        return view('admin.users.profile')->with('profile',$profile);
+    }
+
+    public function profileUpdate(Request $request,$id){
+        // return $request->all();
+        $user=User::findOrFail($id);
+        $data=$request->all();
+        $status=$user->fill($data)->save();
+        if($status){
+            request()->session()->flash('success','Successfully updated your profile');
+        }
+        else{
+            request()->session()->flash('error','Please try again!');
+        }
+        return redirect()->back();
+    }
+
+    public function changePassword(){
+        return view('admin.layouts.changePassword');
+    }
+    public function changPasswordStore(Request $request)
+    {
+        $request->validate([
+            'current_password' => ['required', new MatchOldPassword],
+            'new_password' => ['required'],
+            'new_confirm_password' => ['same:new_password'],
+        ]);
+   
+        User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
+   
+        return redirect()->route('admin')->with('success','Password successfully changed');
     }
 }
